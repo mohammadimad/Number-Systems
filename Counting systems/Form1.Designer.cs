7c9bee8d61ae58f0420a21c62beedee8f1003291
mohammad Abddelfattah <72163221+mohammadimad@users.noreply.github.com>
2023-07-26 20:04:36 +0300

    Add files via upload

---
+++ Counting systems/Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿
+namespace Counting_systems
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.txtNumber = new System.Windows.Forms.TextBox();
+            this.lblTo = new System.Windows.Forms.Label();
+            this.lblFrom = new System.Windows.Forms.Label();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.FromComboBox = new System.Windows.Forms.ComboBox();
+            this.ToComboBox = new System.Windows.Forms.ComboBox();
+            this.btnSwap = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnRestart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtResult
+            // 
+            this.txtResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtResult.Location = new System.Drawing.Point(148, 261);
+            this.txtResult.Multiline = true;
+            this.txtResult.Name = "txtResult";
+            this.txtResult.ReadOnly = true;
+            this.txtResult.Size = new System.Drawing.Size(439, 43);
+            this.txtResult.TabIndex = 0;
+            // 
+            // txtNumber
+            // 
+            this.txtNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNumber.Location = new System.Drawing.Point(148, 163);
+            this.txtNumber.Multiline = true;
+            this.txtNumber.Name = "txtNumber";
+            this.txtNumber.ReadOnly = true;
+            this.txtNumber.Size = new System.Drawing.Size(439, 37);
+            this.txtNumber.TabIndex = 1;
+            // 
+            // lblTo
+            // 
+            this.lblTo.AutoSize = true;
+            this.lblTo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTo.Location = new System.Drawing.Point(86, 26);
+            this.lblTo.Name = "lblTo";
+            this.lblTo.Size = new System.Drawing.Size(27, 20);
+            this.lblTo.TabIndex = 2;
+            this.lblTo.Text = "To";
+            // 
+            // lblFrom
+            // 
+            this.lblFrom.AutoSize = true;
+            this.lblFrom.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFrom.Location = new System.Drawing.Point(528, 26);
+            this.lblFrom.Name = "lblFrom";
+            this.lblFrom.Size = new System.Drawing.Size(46, 20);
+            this.lblFrom.TabIndex = 3;
+            this.lblFrom.Text = "From";
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConvert.Location = new System.Drawing.Point(134, 357);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(135, 61);
+            this.btnConvert.TabIndex = 4;
+            this.btnConvert.Text = "Convert";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // FromComboBox
+            // 
+            this.FromComboBox.FormattingEnabled = true;
+            this.FromComboBox.Items.AddRange(new object[] {
+            "Decimal",
+            "Binary",
+            "Octal",
+            "Hexadecimal"});
+            this.FromComboBox.Location = new System.Drawing.Point(507, 74);
+            this.FromComboBox.Name = "FromComboBox";
+            this.FromComboBox.Size = new System.Drawing.Size(165, 21);
+            this.FromComboBox.TabIndex = 5;
+            this.FromComboBox.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // ToComboBox
+            // 
+            this.ToComboBox.FormattingEnabled = true;
+            this.ToComboBox.Items.AddRange(new object[] {
+            "Decimal",
+            "Binary",
+            "Octal",
+            "Hexadecimal"});
+            this.ToComboBox.Location = new System.Drawing.Point(12, 74);
+            this.ToComboBox.Name = "ToComboBox";
+            this.ToComboBox.Size = new System.Drawing.Size(165, 21);
+            this.ToComboBox.TabIndex = 6;
+            this.ToComboBox.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // btnSwap
+            // 
+            this.btnSwap.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.btnSwap.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSwap.Location = new System.Drawing.Point(306, 36);
+            this.btnSwap.Name = "btnSwap";
+            this.btnSwap.Size = new System.Drawing.Size(107, 85);
+            this.btnSwap.TabIndex = 7;
+            this.btnSwap.Text = "Swap";
+            this.btnSwap.UseVisualStyleBackColor = false;
+            this.btnSwap.Click += new System.EventHandler(this.btnSwap_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClose.Location = new System.Drawing.Point(326, 357);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(135, 61);
+            this.btnClose.TabIndex = 8;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // btnRestart
+            // 
+            this.btnRestart.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRestart.Location = new System.Drawing.Point(495, 357);
+            this.btnRestart.Name = "btnRestart";
+            this.btnRestart.Size = new System.Drawing.Size(135, 61);
+            this.btnRestart.TabIndex = 9;
+            this.btnRestart.Text = "Restart";
+            this.btnRestart.UseVisualStyleBackColor = true;
+            this.btnRestart.Click += new System.EventHandler(this.btnRestart_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Aqua;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnRestart);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnSwap);
+            this.Controls.Add(this.ToComboBox);
+            this.Controls.Add(this.FromComboBox);
+            this.Controls.Add(this.btnConvert);
+            this.Controls.Add(this.lblFrom);
+            this.Controls.Add(this.lblTo);
+            this.Controls.Add(this.txtNumber);
+            this.Controls.Add(this.txtResult);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.TextBox txtNumber;
+        private System.Windows.Forms.Label lblTo;
+        private System.Windows.Forms.Label lblFrom;
+        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.ComboBox FromComboBox;
+        private System.Windows.Forms.ComboBox ToComboBox;
+        private System.Windows.Forms.Button btnSwap;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button btnRestart;
+    }
+}
+
---
+++ Counting systems/Form1.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Counting_systems
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void ReStrat()
+        {
+            txtNumber.ReadOnly = true;
+            txtNumber.Text = "";
+            
+            FromComboBox.SelectedIndex = 0;
+            ToComboBox.SelectedIndex = 1;
+            txtResult.Text = "";
+
+        }
+        private void CheckNumericInput(int num,int count1,int x, int y,string word = "binary")
+        {
+            while (count1 != 0)
+            {
+                int z = count1 % 10;
+                count1 /= 10;
+                if (!(z >= x && z <= y))
+                {
+                    MessageBox.Show("Please enter a valid "+ word +" number.");
+                    ReStrat();
+                    
+                    break;
+                }
+
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            
+            string result = "";
+            
+            switch (FromComboBox.Text)
+            {
+                case "Decimal":
+                    int num = int.Parse(txtNumber.Text);
+                    switch (ToComboBox.Text) {
+                        case "Binary":
+                            result = Convert.ToString(num, 2);
+                            break;
+                        case "Octal":
+                            result = Convert.ToString(num, 8);
+                            break;
+                        case "Hexadecimal":
+                            result = Convert.ToString(num, 16);
+                            break;
+                    }
+                    break;
+                case "Binary":
+                  
+                    int num1 = int.Parse(txtNumber.Text);
+                    int count1 = num1;
+                    CheckNumericInput(num1, count1, 0, 1);
+                    
+                    switch (ToComboBox.Text)
+                    {
+                        case "Decimal":
+                            num1 = Convert.ToInt32(txtNumber.Text, 2);
+                            result = num1.ToString();
+                            break;
+                        case "Octal":
+                            result = Convert.ToString(num1, 8);
+                            break;
+                        case "Hexadecimal":
+                            result = Convert.ToString(num1, 16);
+                            break;
+                    }
+                    break;
+                    
+                case "Octal":
+                    int num2= int.Parse(txtNumber.Text);
+                    int count2 = num2;
+                    CheckNumericInput(num2, count2, 0, 7,"octal");
+                   
+                        switch (ToComboBox.Text)
+                        {
+                            case "Decimal":
+                                num2 = Convert.ToInt32(txtNumber.Text, 8);
+                                result = num2.ToString(); ;
+                                break;
+                            case "Binary":
+                                result = Convert.ToString(num2, 2);
+                                break;
+                            case "Hexadecimal":
+                                result = Convert.ToString(num2, 16);
+                                break;
+                        }
+                       break;
+                case "Hexadecimal":
+                    
+                    int num3 = int.Parse(txtNumber.Text);
+                     switch (ToComboBox.Text)
+                        {
+                            case "Decimal":
+                                num3 = Convert.ToInt32(txtNumber.Text, 16);
+                                result = num3.ToString();
+                                break;
+                            case "Binary":
+                                result = Convert.ToString(num3, 2);
+                                break;
+                            case "Octal":
+                                result = Convert.ToString(num3, 8);
+                                break;
+                        }
+                                     break;
+            }
+            
+            txtResult.Text = result;
+        }
+       
+            
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            txtNumber.ReadOnly = false;
+            if (FromComboBox.SelectedIndex == 1)
+                ToComboBox.SelectedIndex = 0;
+            else
+                ToComboBox.SelectedIndex = 1;
+
+          
+            lblFrom.Text = FromComboBox.SelectedItem.ToString();
+        }
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            lblTo.Text = ToComboBox.SelectedItem.ToString();
+
+        }
+
+        private void btnSwap_Click(object sender, EventArgs e)
+        {
+            string temp = FromComboBox.Text;
+            FromComboBox.Text = ToComboBox.Text;
+            ToComboBox.Text = temp;
+        }
+
+       
+
+        private void btnRestart_Click(object sender, EventArgs e)
+        {
+            txtNumber.Text = "";
+            txtNumber.ReadOnly = true;
+            txtResult.Text = "";
+            FromComboBox.SelectedIndex = 0;
+            ToComboBox.SelectedIndex = 1;
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            FromComboBox.SelectedIndex = 0;
+            ToComboBox.SelectedIndex = 1;
+        }
+    }
+}
